ba4972cb94fbf7e796d1583c50e400d63e8eb29c
fengtingting <fengtingting@cecb2b.com>
2016-08-26 17:25:18 +0800

    顶部广告轮播

--- js/base.js
+++ js/base.js
@@ -175,3 +175,29 @@
         $($(".mnfloor li").get(0)).addClass("current"); 
     }
 }
+
+// 顶部上下轮播
+                function startmarquee(lh,speed,delay) {
+                var p=false;
+                var t;
+                var o=document.getElementById("marqueebox");
+                o.innerHTML+=o.innerHTML;
+                o.style.marginTop=0;
+                o.onmouseover=function(){p=true;}
+                o.onmouseout=function(){p=false;}
+                function start(){
+                t=setInterval(scrolling,speed);
+                if(!p) o.style.marginTop=parseInt(o.style.marginTop)-1+"px";
+                }
+                function scrolling(){
+                if(parseInt(o.style.marginTop)%lh!=0){
+                o.style.marginTop=parseInt(o.style.marginTop)-1+"px";
+                if(Math.abs(parseInt(o.style.marginTop))>=o.scrollHeight/1) o.style.marginTop=0;
+                }else{
+                clearInterval(t);
+                setTimeout(start,delay);
+                }
+                }
+                setTimeout(start,delay);
+                }
+                startmarquee(30,30,1500);
