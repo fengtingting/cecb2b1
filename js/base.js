dda9bccb5ad7dda862f7313976ddff1ed4d7f637
fengtingting <fengtingting@cecb2b.com>
2016-08-26 14:16:30 +0800

    收索框调整

--- js/base.js
+++ js/base.js
@@ -77,42 +77,16 @@
            target.parents('.select').addClass('ihide').prev()[0].innerText=value;
            setTimeout(function(){
                 target.parents('.select').removeClass('ihide');
-           })
-
-        })
+           });
+        });
         $('#J_inquirySuperBuyer .tab-c').each(function(index, item) {
             var lr = new tt.carouselLR({
                 el: item,
                 item: 'li',
                 iconl: '.czleftbut',
                 iconr: '.czrightbut'
-            })
-        })
-
-         //通用咨询
-                $(".zx li").mouseenter(function(){
-                    $(this).children("div").show();
-                })
-                $(".zx li").mouseleave(function(){
-                    $(this).children("div").stop().hide();
-                })  
-                $(window).scroll(function(){
-                    var A = $(window).scrollTop();
-
-                    if(A > 400){
-                        $("#back").css("visibility","visible");
-                    }else{
-                        $("#back").css("visibility","hidden");
-                    }
-                });
-                $("#back").click(function(){
-                    $("html,body").animate(
-                        {
-                            "scrollTop":0
-                        },500
-                    );  
-                })
-                
+            });
+        });       
          var left=$('.container').eq(0).offset().left-111;
         $("#elevator").css('left',left);
         var floorSet = new Array();
